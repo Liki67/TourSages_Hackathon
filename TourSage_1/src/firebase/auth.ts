19647521d0228b17cf8d7bd456b11b9cca5e577f
N S Paranjay Reddy <155084927+Paranjay-pandu@users.noreply.github.com>
2025-04-12 06:30:38 +0530

    Delete TourSage_1 directory and making it ready for re-upload

--- TourSage_1/src/firebase/auth.ts
+++
@@ -1,8 +0,0 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from './config';
-
-export const signInWithGoogle = async () => {
-  const provider = new GoogleAuthProvider();
-  const result = await signInWithPopup(auth, provider);
-  return result.user;
-};
